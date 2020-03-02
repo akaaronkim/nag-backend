a52ab1950123c328f1240432380366062d89d6f4
Girija Viswanathan <58586934+GirijaViswa@users.noreply.github.com>
2020-03-02 15:01:49 -0800

    User authentication

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,12 +1,24 @@
 class UserController < ApplicationController
-    def create
-        user = User.find_by(username:params[:username])
+#Authenticate the user
+    def check
+        user = User.find_by(name:params[:username])
         if user && user.authenticate(params[:password])
             my_token = issue_token(user)
-            render json: {id: user.id, username: user.username, token: my_token}
-          else
+            render json: {id: user.id, username: user.name, token: my_token}
+        else
             render json: {error: 'The user could not be found'}, status: 401
-          end
+        end
+    end
+
+#Create a new user
+    def create
+        user = User.new()
+        if user.save
+            byebug #=> Check for params
+            #call for check
+            render json: {id: user.id, username: user.name, token: my_token}
+        else
+            render json: {error: 'The user could not be created'}, status: 401
         end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  post '/auth', to: 'user#create'
+  post '/auth', to: 'user#check'        #Authenticate the user
+  post '/new', to: 'user#create'        #Create a new user
+  
 end
