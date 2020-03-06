0e4327c366b40339ff3387b744d57d246c41ccb0
Girija Viswanathan <58586934+GirijaViswa@users.noreply.github.com>
2020-03-06 09:11:23 -0800

    order posting in backend

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -4,11 +4,16 @@ class OrderController < ApplicationController
         #fetch the user
         user = User.find_by(name:params[:username])
         #fetch the payment info
-        payment_info = PaymentInfo.find_by(first_name:)
+        # payment_info = PaymentInfo.find_by(first_name:)
         #Create a new order
         #Check for token
-        order = Order.new(delivery_address: ,pickup_address: , order_date: , order_time: , delivery_date: , delivery_time: , fee: , status: , user_id: user.id, payment_info_id: payment_info.id)
+        # byebug
+        order = Order.new(delivery_address: params[:delivery_address],pickup_address: params[:pickup_address], order_date: params[:order_date] , order_time: params[:order_time] , delivery_date: params[:delivery_date] , delivery_time: params[:delivery_time] , fee: params[:fee] , status: params[:status] , user_id: params[:user_id], payment_info_id: params[:payment_info_id])
+        # byebug
         if order.save
+            render json: {id: order.id}
+        else
+            render json: {error: 'Contact support team for further details'}, status: 401
         end
     end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -23,10 +23,11 @@ user7 =  User.create(name: "donny", email: "donny@hotmail.com", address_one: "14
 user8 = User.create(name: "david",email: "david@gmail.com", address_one: "1460 mission street", address_two: "floor one", city: "San Francisco", state: "California", zipcode: "94106",password: "password")
 
 #Payment details
-p1 = PaymentInfo.create(card_grid1: 4833, card_grid2: 1600, card_grid3: 3368, card_grid4: 2222, first_name: "aaron", last_name: "kim", expiration_date: "2022-12-31", cvv: 248, card_type: "visa", user:user1  )
+p12 = PaymentInfo.create(card_grid1: 4833, card_grid2: 1600, card_grid3: 3368, card_grid4: 2222, first_name: "aaron", last_name: "kim", expiration_date: "2022-12-31", cvv: 248, card_type: "visa", user:user1  )
 p2 = PaymentInfo.create(card_grid1: 1111, card_grid2: 2222, card_grid3: 3333, card_grid4: 4444, first_name: "nick", last_name: "corona", expiration_date: "2022-12-31", cvv: 123, card_type: "visa", user:user3  )
 p3 = PaymentInfo.create(card_grid1: 4833, card_grid2: 1600, card_grid3: 1234, card_grid4: 1234, first_name: "bronte", last_name: "sewell", expiration_date: "2022-12-31", cvv: 234, card_type: "visa", user:user4  )
 p4 = PaymentInfo.create(card_grid1: 4833, card_grid2: 1600, card_grid3: 3368, card_grid4: 9090, first_name: "david", last_name: "rosenholz", expiration_date: "2022-12-31", cvv: 643, card_type: "visa", user:user8  )
+p1 = PaymentInfo.create(card_grid1: 4242, card_grid2: 4242, card_grid3: 4242, card_grid4: 4242, first_name: "User", last_name: "User", expiration_date: "2022-12-31", cvv: 643, card_type: "visa", user:user1  )
 
 #Order details
 Order.create(delivery_address: "548 Market St, San Francisco, CA 94104",pickup_address: "1460 Mission St, San Francisco, CA 94106", order_date: "2020-02-28", order_time: "15:31:26", delivery_date: "2020-02-28", delivery_time: "16:31:26", fee: 3.26, status: "successful", user_id: user1.id, payment_info_id: p1.id)
