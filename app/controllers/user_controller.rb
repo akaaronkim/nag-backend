49c573cee7b238bee1dca8cd5b3394eddf53ee88
coronaisme <coronaisme@gmail.com>
2020-03-05 16:56:43 -0800

    needed zipcode

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,7 +4,7 @@ class UserController < ApplicationController
         user = User.find_by(name:params[:username])
         if user && user.authenticate(params[:password])
             my_token = issue_token(user)
-            render json: {id: user.id, username: user.name, token: my_token}
+            render json: {id: user.id, username: user.name,zipcode: user.zipcode, token: my_token}
         else
             render json: {error: 'The user could not be found'}, status: 401
         end
@@ -15,7 +15,7 @@ class UserController < ApplicationController
         user = User.new(name: params[:name], email: params[:email], address_one:params[:address_one] , address_two: params[:address_two], city: params[:city], state: params[:state], zipcode: params[:zipcode], password: params[:password])
         if user.save
             my_token = issue_token(user)
-            render json: {id: user.id, username: user.name, token: my_token}
+            render json: {id: user.id, username: user.name, zipcode: user.zipcode, token: my_token}
         else
             render json: {error: 'The user could not be created'}, status: 401
         end
